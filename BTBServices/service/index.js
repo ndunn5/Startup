47d5f7f22efa98071c1c60c6ddbaa835210c5cad
Noah Dunn <ndunn5@byu.edu>
2024-11-18 15:01:42 -0700

    Now it is showing up but hmmmmm.

--- BTBServices/service/index.js
+++ BTBServices/service/index.js
@@ -7,7 +7,7 @@ const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 app.use(cors()); 
 
-app.use(express.static('../public'));
+app.use(express.static('public'));
 
 var apiRouter = express.Router();
 app.use('/api', apiRouter);
@@ -16,6 +16,7 @@ apiRouter.get('/random-song', async (_req, res) => {
   try {
     const response = await axios.get('https://energy.ch/api/channels/bern/playouts');
     const songs = response.data;
+    console.log('Fetching data from API...');
 
     if (!songs || songs.length === 0) {
       return res.status(404).send({ msg: 'No songs found' });
